5151bab39ea099b51a369280bf29a3e0848c27f4
nagashree <naagashree42@gmail.com>
2026-05-15 05:25:53 +0530

    done

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,15 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	 System.out.println( "Hello Nagashreeeee" );
+
+        System.out.println( "Hello World!" );
+    }
+}
